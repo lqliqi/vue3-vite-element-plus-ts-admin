49566b4b826e8f050e8f41ccfd5972dc122c503c
lqq2 <lqq2@meitu.com>
2021-10-08 15:53:12 +0800

    表格样式调整

--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -23,13 +23,13 @@
       :list="tableData"
       @update="update"
     >
-      <el-table-column fixed prop="date" label="Date" width="150" />
-      <el-table-column prop="name" label="Name" width="120" />
-      <el-table-column prop="state" label="State" width="120" />
-      <el-table-column prop="city" label="City" width="120" />
+      <el-table-column prop="date" label="Date" width="200" />
+      <el-table-column prop="name" label="Name" width="200" />
+      <el-table-column prop="state" label="State" width="200" />
+      <el-table-column prop="city" label="City" width="200" />
       <el-table-column prop="address" label="Address" width="600" />
-      <el-table-column prop="zip" label="Zip" width="120" />
-      <el-table-column fixed="right" label="Operations" width="120">
+      <el-table-column prop="zip" label="Zip" width="200" />
+      <el-table-column fixed="right" label="Operations" width="200">
         <template #default>
           <el-button type="text" size="small" @click="handleClick('Detail')">Detail</el-button>
           <el-button type="text" size="small" @click="handleClick('Edit')">Edit</el-button>
