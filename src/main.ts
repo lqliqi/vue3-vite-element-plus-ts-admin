3f9958780c9236f7e618613f6d3327f28d54b5cc
lqq2 <lqq2@meitu.com>
2021-10-09 16:10:53 +0800

    引入语言包使element plus 分页显示为中文

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import locale from 'element-plus/lib/locale/lang/zh-cn' // element-plus
-
 import App from './App.vue'
 import router from './router/index.ts'
 import store from './store/index.ts'
